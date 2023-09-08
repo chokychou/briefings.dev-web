795ea82190a62b784348468c115a5fdfee4433aa
chokychou <nobreakfast@foxmail.com>
2023-09-08 01:32:48 -0700

    nav does not cover banner

--- src/app/Nav.tsx
+++ src/app/Nav.tsx
@@ -15,7 +15,7 @@ export default function Nav() {
     }
 
     return (
-        <div>
+        <div className="">
             <button type="button" id="navigation" onClick={() => navigation_transition()} >
                 <label htmlFor="navigation" className="fixed bottom-0 left-0 z-50 flex items-center justify-center w-12 h-12 mb-4 ml-4 bg-white border rounded-full shadow-lg cursor-pointer text-slate-600 border-slate-300 lg:hidden transition duration-200 ease-in-out active:scale-95">
                     <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
@@ -24,7 +24,7 @@ export default function Nav() {
                 </label>
             </button>
 
-            <div id="nav-div" className="fixed top-0 left-0 z-20 w-64 h-full transition-all duration-500 transform -translate-x-full bg-white shadow-lg lg:translate-x-0">
+            <div id="nav-div" className="fixed top-5 left-0 z-20 w-64 h-full transition-all duration-500 transform -translate-x-full bg-white shadow-lg lg:translate-x-0">
                 <nav className="sticky top-[4.5rem] w-64 text-base lg:text-sm xl:w-72">
                     <ul role="list" className="-ml-0.5 h-[calc(100vh-4.5rem)] overflow-y-auto py-7 pl-0.5 pr-8 space-y-8 xl:pr-16">
                         <li>
